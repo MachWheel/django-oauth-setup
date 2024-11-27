a37931dd4b94a6b7a5fe98fc4a8ba7ffd1db534b
wyller <wyller22@gmail.com>
2024-11-27 00:49:42 -0300

    updated to-dos

--- users/models/auth_token.py
+++ users/models/auth_token.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class AuthToken(models.Model):
     """
-    TODO: not implemented; replace with yours or use 'django-rest-knox' if you need:
+    TODO: not implemented;
+          replace with your token model
+          or use 'django-rest-knox' if you need:
           https://jazzband.github.io/django-rest-knox/
     """
     pass
